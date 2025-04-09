4a1a97cf92b5943e41eeb7113201a6e2d943efd4
aswath <aswath@veersoftsolutions.com>
2025-04-09 12:15:06 +0000

    env file is changed to .env

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const rateLimit = require('express-rate-limit');
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.config({ path: '.env.example' });
+dotenv.config({ path: '.env' });
 
 /**
  * Set config values
